908a49aa8e91fd4d636257bb3aaee32635c6b493
amierfaudzi <amier.faudzi@gmail.com>
2021-01-11 11:35:15 -0500

    update navigation component to handle getting the user data

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,20 +3,40 @@ import './Navigation.scss';
 import { Link, useHistory } from 'react-router-dom';
 import { ReactComponent as Forge } from '../../assets/icons/icons8-hammer-and-anvil.svg';
 import { UserContext } from '../../contexts/UserContext';
+import { ProfileContext } from '../../contexts/ProfileContext';
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
+import { SET_LOADING, SET_USER } from '../../reducers/types';
 
 export default function Navigation() {
 
     const history = useHistory();
-    const { user, handleAuth } = useContext(UserContext);
+    const token = localStorage.FBIdToken;
+    const { user, dispatch } = useContext(ProfileContext);
 
     const handleLogout = () => {
         localStorage.removeItem('FBIdToken');
         delete axios.defaults.headers.common['Authorization'];
-        handleAuth(null, true);
+        //handleAuth(null, true);
         history.push('/');
     }
+
+    useEffect(()=>{
+        if(token){
+            const decodedToken =jwtDecode(token);
+            if(decodedToken.exp*1000 < Date.now()){
+                window.location.href = '/login';
+            } else {
+                dispatch({ type: SET_LOADING});
+                axios.defaults.headers.common['Authorization'] = token;
+                axios.get('/user')
+                .then(res => {
+                    dispatchEvent({ type: SET_USER, name: res.data.credentials.name})
+                })
+                .catch(err => console.log(err));
+            }
+        }
+    }, [user.token])
 
     return (
 
